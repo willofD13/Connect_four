a0a083c9d5d749c52e242513e8172165535ae764
willofD13 <leonidastsiakas13@gmail.com>
2023-09-13 16:11:04 +0300

    initialize instance variables

--- lib/connect_four.rb
+++ lib/connect_four.rb
@@ -1,2 +1,8 @@
 class ConnectFour
+    def initialize(player_1 = nil, player_2 = nil)
+        @player_1 = player_1 
+        @player_2 = player_2 
+        @turn = 1 
+        @grid = Array.new(6) {Array.new(7, '_')}
+    end
 end
