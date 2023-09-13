39a4ede80ef93494176f62755a18b8cb226cc241
willofD13 <leonidastsiakas13@gmail.com>
2023-09-13 16:33:58 +0300

    Write get and set methhods for player names

--- lib/connect_four.rb
+++ lib/connect_four.rb
@@ -1,4 +1,6 @@
 class ConnectFour
+    attr_accessor :player_1, :player_2
+
     def initialize(player_1 = nil, player_2 = nil, grid = Array.new(6) {Array.new(7, '_')}, turn = 1)
         @player_1 = player_1 
         @player_2 = player_2 
