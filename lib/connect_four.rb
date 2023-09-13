6d52f4454bbb05ac759b07fdb61bc6c5bebadf82
willofD13 <leonidastsiakas13@gmail.com>
2023-09-13 16:40:49 +0300

    display game board

--- lib/connect_four.rb
+++ lib/connect_four.rb
@@ -7,4 +7,13 @@ class ConnectFour
         @turn = turn
         @grid = grid
     end
+
+    def display_board(grid)
+        puts "#{@grid[0][0]} | #{@grid[0][1]} | #{@grid[0][2]} | #{@grid[0][3]} | #{@grid[0][4]} | #{@grid[0][5]} | #{@grid[0][6]}"
+        puts "#{@grid[1][0]} | #{@grid[1][1]} | #{@grid[1][2]} | #{@grid[1][3]} | #{@grid[1][4]} | #{@grid[1][5]} | #{@grid[1][6]}"
+        puts "#{@grid[2][0]} | #{@grid[2][1]} | #{@grid[2][2]} | #{@grid[2][3]} | #{@grid[2][4]} | #{@grid[2][5]} | #{@grid[2][6]}"
+        puts "#{@grid[3][0]} | #{@grid[3][1]} | #{@grid[3][2]} | #{@grid[3][3]} | #{@grid[3][4]} | #{@grid[3][5]} | #{@grid[3][6]}"
+        puts "#{@grid[4][0]} | #{@grid[4][1]} | #{@grid[4][2]} | #{@grid[4][3]} | #{@grid[4][4]} | #{@grid[4][5]} | #{@grid[4][6]}"
+        puts "#{@grid[5][0]} | #{@grid[5][1]} | #{@grid[5][2]} | #{@grid[5][3]} | #{@grid[5][4]} | #{@grid[5][5]} | #{@grid[5][6]}"
+    end
 end
