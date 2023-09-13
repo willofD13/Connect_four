04e86ca960d8f8c15e10a6c87b246eb9fb72d938
willofD13 <leonidastsiakas13@gmail.com>
2023-09-13 17:54:44 +0300

    Test the player's input method

--- spec/connect_four_spec.rb
+++ spec/connect_four_spec.rb
@@ -8,4 +8,23 @@ describe ConnectFour do
             ConnectFour.new
         end 
     end 
+
+    describe "player_choice" do
+        subject(:game_choice) { described_class.new}
+
+        context "when player gives valid input" do 
+            before do 
+                valid_input = '5 6'
+                allow(game_choice).to receive(:gets).and_return(valid_input)
+            end
+
+            it "exits the loop when given valid input" do
+                name = 'leon'
+                symbol = 'O'
+                error_message = "Please insert space between coordinates"
+                expect(game_choice).not_to receive(:puts).with(error_message)
+                game_choice.player_choice(name,symbol)
+            end
+        end
+    end
 end
