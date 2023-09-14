69426cfe150e649af9204e582858eae2ea75229a
willofD13 <leonidastsiakas13@gmail.com>
2023-09-14 19:36:58 +0300

    Tie all methods together to play the game

--- lib/connect_four.rb
+++ lib/connect_four.rb
@@ -11,7 +11,7 @@ class ConnectFour
     end
     
 
-    def display_board(grid)
+    def display_board
         puts "#{@grid[0][0]} | #{@grid[0][1]} | #{@grid[0][2]} | #{@grid[0][3]} | #{@grid[0][4]} | #{@grid[0][5]} | #{@grid[0][6]}"
         puts "#{@grid[1][0]} | #{@grid[1][1]} | #{@grid[1][2]} | #{@grid[1][3]} | #{@grid[1][4]} | #{@grid[1][5]} | #{@grid[1][6]}"
         puts "#{@grid[2][0]} | #{@grid[2][1]} | #{@grid[2][2]} | #{@grid[2][3]} | #{@grid[2][4]} | #{@grid[2][5]} | #{@grid[2][6]}"
@@ -20,7 +20,7 @@ class ConnectFour
         puts "#{@grid[5][0]} | #{@grid[5][1]} | #{@grid[5][2]} | #{@grid[5][3]} | #{@grid[5][4]} | #{@grid[5][5]} | #{@grid[5][6]}"
     end
 
-    def introduction(player1,player2)
+    def introduction
         puts "Welcome to connect four!"
         puts "Player 1, please insert your name"
         @player_1 = gets.chomp
@@ -30,6 +30,21 @@ class ConnectFour
         there is no emprty space beneath it. First coordinate should be between 
         0 and 5, second coordinate should be netween 0 and 6. The two coordinates 
         should separated by space. Have fun!"
+    end
+
+    def play_game
+        until @turn == 43 do 
+            coordinates = player_choice(@player_1) if @turn.odd?
+            coordinates = player_choice(@player_2) if @turn.even?
+            first_coord = isolate_coordinate(coordinates,0)
+            second_coord = isolate_coordinate(coordinates,1)
+            add_to_board(first_coord,second_coord,'O') if @turn.odd?
+            add_to_board(first_coord,second_coord,'X') if @turn.even?
+            display_board
+            flattened_grid = @grid.flatten 
+            check_for_winner(flattened_grid)
+            @turn += 1
+        end 
     end
 
     def player_choice(player_name)
@@ -60,34 +75,42 @@ class ConnectFour
         array.each_with_index do |v,i| 
             if  v == "O" && array[i + 6] == v && array[i + 12] == v && array[i + 18] == v
                 puts "The winner is #{@player_1}"
+                @turn = 43
                 break
             elsif
                 v == "X" && array[i + 6] == v && array[i + 12] == v && array[i + 18] == v
                 puts "The winner is #{@player_2}"
+                @turn = 43
                 break
             elsif 
                 v == 'O' && array[i + 8] == v && array[i + 16] == v && array[i + 24] == v
                 puts "The winner is #{@player_1}"
+                @turn = 43
                 break
             elsif 
                 v == "X" && array[i + 8] == v && array[i + 16] == v && array[i + 24] == v
                 puts "The winner is #{@player_2}"
+                @turn = 43
                 break
             elsif
                 v == "O" && array[i + 7] == v && array[i + 14] == v && array[i + 21] == v
                 puts "The winner is #{@player_1}"
+                @turn = 43
                 break
             elsif
                 v == "X" && array[i + 7] == v && array[i + 14] == v && array[i + 21] == v
                 puts "The winner is #{@player_2}"
+                @turn = 43
                 break
             elsif 
                 v == "O" && array[i + 1] == v && array[i + 2] == v && array[i + 3] == v
                 puts "The winner is #{@player_1}"
+                @turn = 43
                 break
             elsif 
                 v == "X" && array[i + 1] == v && array[i + 2] == v && array[i + 3] == v
                 puts "The winner is #{@player_2}"
+                @turn = 43
                 break
             else
              nil   
@@ -96,3 +119,8 @@ class ConnectFour
     end
 
 end
+
+game = ConnectFour.new
+game.introduction
+game.display_board
+game.play_game
